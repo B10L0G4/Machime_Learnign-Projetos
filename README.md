3604be21e9adbf057238c032d5e106760981f2c5
B1olog4 <vandyyh@gmail.com>
2023-02-14 21:14:02 -0300

    labelEncoder and preprocessing

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Machine_Learning-Projetos
 Projetos de estudo com Machine Learning
+
+Este projeto tem como objetivo classificar temperaturas 
+
+criar um modelo sem explicitamente definir a regra para classificar as temperaturas. O própio modelo irá aprender a classifcar as temperaturas. 
+
+O modelo será treinado com dados de tempaturas ficticias.
+ 
--- projeto.py
+++ projeto.py
@@ -1,5 +1,20 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
 
 df= pd.read_csv('./arquivos/temperature.csv')
 
-print(df.head())
+#extração de x e y que serão variareis preditoras que iremos usar e as variaveis target que queremos  usar prever - será usado apenas a temperatura para esses projeto. 
+
+# a classe LabelEncoder é usada para transformar os valores de uma coluna em valores numericos
+
+
+x, y = df[['temperatura']],df[['classification']].values
+
+print("x:\n",x)
+print("y:\n",y)
+
+le = LabelEncoder()
+y = le.fit_transform(y.ravel()) #rave() transforma o array em uma linha, fit_transform( transforma os valores em numericos )
+
+print("y:\n",y)
